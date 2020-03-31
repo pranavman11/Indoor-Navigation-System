c6a8b360b258427cdec74bccfadb46c570d0229e
Vedant Khairnar <vedron007@gmail.com>
2020-03-31 22:15:31 +0530

    code 1.0

---
+++ Assets/Scripts/ARTapToPlaceObject.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.Experimental.XR;
+using System;
+
+public class ARTapToPlaceObject : MonoBehaviour
+{
+    public GameObject objectToPlace;
+    public GameObject placementIndicator;
+
+    private ARSessionOrigin arOrigin;
+    private Pose placementPose;
+    private bool placementPoseIsValid = false;
+
+    void Start()
+    {
+        arOrigin = FindObjectOfType<ARSessionOrigin>();
+    }
+
+    void Update()
+    {
+        UpdatePlacementPose();
+        UpdatePlacementIndicator();
+
+        if (placementPoseIsValid && Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+        {
+            PlaceObject();
+        }
+    }
+
+    private void PlaceObject()
+    {
+        Instantiate(objectToPlace, placementPose.position, placementPose.rotation);
+    }
+
+    private void UpdatePlacementIndicator()
+    {
+        if (placementPoseIsValid)
+        {
+            placementIndicator.SetActive(true);
+            placementIndicator.transform.SetPositionAndRotation(placementPose.position, placementPose.rotation);
+        }
+        else
+        {
+            placementIndicator.SetActive(false);
+        }
+    }
+
+    private void UpdatePlacementPose()
+    {
+      var raycastManager = FindObjectOfType<ARRaycastManager>();
+var screenCenter = Camera.main.ViewportToScreenPoint(new Vector3(0.5f, 0.5f));
+var hits = new List<ARRaycastHit>();
+
+var planeManager = GetComponent<ARPlaneManager>();
+
+    raycastManager.Raycast(screenCenter, hits);
+    placementPoseIsValid = hits.Count > 0;
+    if (placementPoseIsValid)
+    {
+        placementPose = hits[0].pose;
+
+        var cameraForward = Camera.current.transform.forward;
+        var cameraBearing = new Vector3(cameraForward.x, 0, cameraForward.z).normalized;
+        placementPose.rotation = Quaternion.LookRotation(cameraBearing);
+    }
+
+
+
+
+    }
+}
